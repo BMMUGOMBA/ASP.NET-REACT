3449ada18aa5de4ec1a0e18a27f6ea8dc5fd9580
Batsirai Mugomba <bmmugomba@gmail.com>
2025-08-28 17:01:22 +0200

    addition

--- reimagination-backend/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ reimagination-backend/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e34b85992a09c6db537ebf8ac386fd1984e1fab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182fc4c62e4d29f55d5e0b38f749124c52db98f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- reimagination-backend/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ reimagination-backend/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7105a26600c5a35407bb36c0473974f082546f17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182fc4c62e4d29f55d5e0b38f749124c52db98f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
