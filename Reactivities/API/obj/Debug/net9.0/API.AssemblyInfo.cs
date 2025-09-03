3a5dd70ac65aa2a0a55e0d8b6ecb1b146912b837
Batsirai Mugomba <bmmugomba@gmail.com>
2025-09-03 12:01:38 +0200

    Project Review Section

--- Reactivities/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Reactivities/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecce445f7ad4b22bcadbd3e4a4ff1a83beb08b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf34e5539984b8afc900f6106c93dfbb2875048")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
