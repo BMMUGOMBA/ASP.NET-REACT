94a70f287c9a8f747a71ffa5cdd7df5f3c40c237
Batsirai Mugomba <bmmugomba@gmail.com>
2025-09-03 16:25:32 +0200

    fetching data on API

--- Reactivities/client/src/App.tsx
+++ Reactivities/client/src/App.tsx
@@ -1,12 +1,29 @@
-
+import { useEffect, useState } from "react";
 
 
 function App() {
+
+  const title = 'Welcome to Reactivities';
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5001/api/activities')
+    .then(response => response.json())
+    .then(data => setActivities(data))
+  },[])
   
 
   return (
-  
-      <h3>Reactivities</h3>
+  <div>
+      <h3 className="app" style={{color:'red'}}>{title}</h3>
+      <ul>
+        {activities.map((activity) => (
+          //<li key={activity.id}>{activity.title}</li>
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+  </div>
+    
    
   )
 }
